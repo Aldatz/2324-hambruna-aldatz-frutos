28492d27138abcb5750e0f9acf4e16c264348d65
Aldatz <aldatz.frutos@ikasle.aeg.eus>
2024-09-13 10:33:11 +0200

    2 ejercicio numero 1 donut con mas azucar

--- main.js
+++ main.js
@@ -4,9 +4,11 @@ const url = "https://gist.githubusercontent.com/Oskar-Dam/62e7175dc542af53a9d18c
 axios.get(url)
   .then(function (response) {
     const data = response.data;
+    //1.
+    sugarDonut(data);
+    //2
 
-    console.log(data);
-    
+
   })
   .catch(function (error) {
     // manejar error
@@ -15,3 +17,36 @@ axios.get(url)
   .finally(function () {
     // siempre sera executado
   });
+
+function sugarDonut(data){
+    let maxSugar = 0;
+    let counter = 0;
+    let donutNames = "";
+    
+    for (i = 0; i < data.items.item.length; i++){
+        if (maxSugar <= parseInt(data.items.item[i].nutrition_facts.nutrition.carbohydrate.carbs_detail.type.sugars)){
+            maxSugar = parseInt(data.items.item[i].nutrition_facts.nutrition.carbohydrate.carbs_detail.type.sugars)
+        }
+    }
+    for (i = 0; i < data.items.item.length; i++){
+        if (maxSugar === parseInt(data.items.item[i].nutrition_facts.nutrition.carbohydrate.carbs_detail.type.sugars)){
+            counter += 1
+        }
+    }
+    for (i = 0; i < data.items.item.length; i++){
+        if (maxSugar === parseInt(data.items.item[i].nutrition_facts.nutrition.carbohydrate.carbs_detail.type.sugars) && counter <= 1){
+            donutNames = data.items.item[i].name;
+        }
+        else{
+            donutNames = donutNames + data.items.item[i].name + " ";
+        }
+    }
+
+    if (counter <= 1){
+        console.log(donutNames + " is the donut whit more sugar");
+    }
+    else{
+        console.log("the donuts whit more sugar are: " + donutNames);
+    }
+    
+  }
